a584fa4c9cca0b0821fd643a9beeb21165ed87c1
Anna Suleri <99440427+ajsuleri@users.noreply.github.com>
2024-07-16 19:10:05 +0200

    Update server.R

--- server.R
+++ server.R
@@ -31,7 +31,7 @@ df5 <- df4 %>%
 # create data results
 data_ib <- data.frame(
   brain_outcome = c('Cerebral lesions', 'White matter lesions', 'White matter microstructure', 'White matter microstructure', 'Anterior cingulate cortex'),
-  finding = c('Positive', 'Positive' ,'Positive', 'Negative', 'Positive'),
+  finding = c('Significant finding', 'Significant finding' ,'Significant finding', 'Null finding', 'Significant finding'),
   modality = c('sMRI', 'sMRI', 'DTI', 'DTI', 'fMRI'),
   count = c(120, 144 ,481, 129, 76),
   author_names = c('Duggan et al, Tian et al', 'Duggan et al, Yoon et al' ,'Conole et al, Sanders et al, Rasmussen et al', 'Conole et al' ,'Spann et al, Goldstein et al'),
@@ -42,7 +42,7 @@ data_ib$finding <- factor(data_ib$finding)
 
 data_ca <- data.frame(
   brain_outcome = c('White matter microstructure', 'White matter microstructure', 'White matter lesions', 'White matter lesions', 'Cerebral lesions', 'Cerebral lesions'),
-  finding = c('Positive', 'Negative','Positive', 'Negative','Positive', 'Negative'),
+  finding = c('Significant finding', 'Null finding','Significant finding', 'Null finding','Significant finding', 'Null finding'),
   modality = c('DTI', 'DTI', 'sMRI', 'sMRI', 'sMRI', 'sMRI'),
   count = c(210, 92, 573, 157, 740, 388),
   author_names = c('Anblagan et al, Gaudet et al, Pogribna et al', 'Chau et al', 'Basu et al, Qin et al, Xu et al, Shevell et al', 'Baud et al, Chau et al, Basu et al', 'Budal et al, Jain et al, Jenster et al', 'Granger et al, Qin et al, Xu et al'),
@@ -53,7 +53,7 @@ data_ca$finding <- factor(data_ca$finding)
 
 data_ci <- data.frame(
   brain_outcome = c('Cerebral lesions', 'Cerebral lesions', 'White matter lesions', 'White matter lesions'),
-  finding = c('Positive', 'Negative', 'Positive', 'Negative'),
+  finding = c('Significant finding', 'Null finding', 'Significant finding', 'Null finding'),
   modality = c('sMRI'),
   count = c(320, 58, 505, 464),
   author_names = c('Kurokawa et al, McKissic et al, Zeng et al, Capretti et al, Oosterom et al, Roee et al, Diebler et al, Turner et al', 'Roee et al, Virkola et al', 'McKissic et al, Capretti et al, Diebler et al', 'Dammann et al'),
@@ -61,27 +61,6 @@ data_ci <- data.frame(
 )
 data_ci$modality <- factor(data_ci$modality, levels = c("sMRI"))
 data_ci$finding <- factor(data_ci$finding)
-
-data_hiv <- data.frame(
-  brain_outcome = c('Cerebral lesions', 'Gray matter volume', 'Cortical surface area', 'White matter volume', 'Basal ganglia', 'White matter microstructure', 'Medial prefrontal cortex', 'Posterior cingulate cortex', 'Right lateral parietal occipital cortices', 'Right middle frontal gyrus', 'Lateral superior frontal gyrus', 'Inferior frontal gyri'),
-  finding = c('Positive','Positive', 'Positive', 'Positive', 'Positive', 'Positive','Positive','Positive','Positive','Positive','Positive','Positive'),
-  modality = c('sMRI', 'sMRI', 'sMRI', 'sMRI', 'sMRI', 'DTI', 'fMRI','fMRI','fMRI','fMRI','fMRI', 'fMRI'),
-  count = c(622, 1182, 1182, 174, 1182, 1182,1182, 1182, 1182, 1182, 1182, 1182),
-  author_names = c('Review by Hoare et al','Review by Martin-Bejarano et al', 'Review by Martin-Bejarano et al', 'Cohen et al, Nwosu et al','Review by Martin-Bejarano et al','Review by Martin-Bejarano et al','Review by Martin-Bejarano et al','Review by Martin-Bejarano et al','Review by Martin-Bejarano et al','Review by Martin-Bejarano et al', 'Review by Martin-Bejarano et al', 'Review by Martin-Bejarano et al'),
-  developmental_period = c('Infancy and child','Child and adolescent', 'Child and adolescent', 'Child', 'Child and adolescent','Child and adolescent','Child and adolescent','Child and adolescent','Child and adolescent','Child and adolescent','Child and adolescent','Child and adolescent')
-)
-data_hiv$modality <- factor(data_hiv$modality, levels = c("sMRI", "DTI", 'fMRI'))
-data_hiv$finding <- factor(data_hiv$finding)
-
-data_zv <- data.frame(
-  brain_outcome = c('Cerebral / white matter lesions'),
-  finding = c('Positive'),
-  modality = c('sMRI'),
-  count = c(2789),
-  author_names = c('All n=20 studies'),
-  developmental_period = c('Infancy')
-)
-data_zv$finding <- factor(data_zv$finding)
 
 # server
 server <- function(input, output) {
@@ -115,7 +94,7 @@ server <- function(input, output) {
              yaxis = list(title = "Sample Size", range = c(0, input$yAxisLimitC)))
   })
   
-  output$plot_ib <- renderPlotly({ plot_ly(data_ib, x = ~brain_outcome, y = ~count, type = 'bar', color = ~finding, colors = c("Positive" = "palegreen3", "Negative" = "brown2"),
+  output$plot_ib <- renderPlotly({ plot_ly(data_ib, x = ~brain_outcome, y = ~count, type = 'bar', color = ~finding, colors = c("Significant finding" = "mediumpurple3", "Null finding" = "orange2"),
                                            text = ~paste('Author Names:', author_names, '<br>Developmental Period:', developmental_period, '<br>Imaging modality:', modality, '<br>N:', count),
                                            hoverinfo = 'text', split = ~finding) %>%
       layout(title = "Summary of findings for exposure: inflammatory biomarkers",
@@ -124,7 +103,7 @@ server <- function(input, output) {
              barmode = 'group')
   })
   
-  output$plot_ca <- renderPlotly({ plot_ly(data_ca, x = ~brain_outcome, y = ~count, type = 'bar', color = ~finding, colors = c("Positive" = "palegreen3", "Negative" = "brown2"),
+  output$plot_ca <- renderPlotly({ plot_ly(data_ca, x = ~brain_outcome, y = ~count, type = 'bar', color = ~finding, colors = c("Significant finding" = "mediumpurple3", "Null finding" = "orange2"),
                                            text = ~paste('Author Names:', author_names, '<br>Developmental Period:', developmental_period, '<br>Imaging modality:', modality,'<br>N:', count),
                                            hoverinfo = 'text', split = ~finding) %>%
       layout(title = "Summary of findings for exposure: chorioamnionitis",
@@ -133,30 +112,12 @@ server <- function(input, output) {
              barmode = 'group')
   })
   
-  output$plot_ci <- renderPlotly({ plot_ly(data_ci, x = ~brain_outcome, y = ~count, type = 'bar', color = ~finding, colors = c("Positive" = "palegreen3", "Negative" = "brown2"),
+  output$plot_ci <- renderPlotly({ plot_ly(data_ci, x = ~brain_outcome, y = ~count, type = 'bar', color = ~finding, colors = c("Significant finding" = "mediumpurple3", "Null finding" = "orange2"),
                                            text = ~paste('Author Names:', author_names, '<br>Developmental Period:', developmental_period, '<br>Imaging modality:', modality,'<br>N:', count),
                                            hoverinfo = 'text', split = ~finding) %>%
       layout(title = "Summary of findings for exposure: common infections",
              xaxis = list(title = "Brain Outcome"),
              yaxis = list(title = "Number of participants", range = c(0, input$yAxisLimitD)),
-             barmode = 'group')
-  })
-  
-  output$plot_hiv <- renderPlotly({ plot_ly(data_hiv, x = ~brain_outcome, y = ~count, type = 'bar', color = ~finding, colors = c("Positive" = "palegreen3", "Negative" = "brown2"),
-                                            text = ~paste('Author Names:', author_names, '<br>Developmental Period:', developmental_period, '<br>Imaging modality:', modality,'<br>N:', count),
-                                            hoverinfo = 'text', split = ~finding) %>%
-      layout(title = "Summary of findings for exposure: HIV",
-             xaxis = list(title = "Brain Outcome"),
-             yaxis = list(title = "Number of participants"),
-             barmode = 'group')
-  })
-  
-  output$plot_zv <- renderPlotly({ plot_ly(data_zv, x = ~brain_outcome, y = ~count, type = 'bar', color = ~finding, colors = c("Positive" = "palegreen3", "Negative" = "brown2"),
-                                           text = ~paste('Author Names:', author_names, '<br>Developmental Period:', developmental_period, '<br>Imaging modality:', modality,'<br>N:', count),
-                                           hoverinfo = 'text', split = ~finding) %>%
-      layout(title = "Summary of findings for exposure: Zika virus",
-             xaxis = list(title = "Brain Outcome"),
-             yaxis = list(title = "Number of participants"),
              barmode = 'group')
   })
 }
