9245dcad8387e1a25a4e4dad87b199ed30bbbe1b
Anna Suleri <99440427+ajsuleri@users.noreply.github.com>
2024-07-21 14:55:41 +0200

    Update server.R

--- server.R
+++ server.R
@@ -10,57 +10,9 @@ library(shinythemes)
 
 # data
 df <- readxl::read_excel("./excel_figure3.xlsx")
-
-# clean up df to be in right format for figure 
-df2 <- rename(df, "Sample size" = N) #rename var name 
-df2$`Publication year` <- as.numeric(df2$`Publication year`) #make numeric 
-df2$Grouping <- as.factor(df2$Grouping) #make factor
-df2 <- rename(df2, 'Group' = Grouping)
-
-# count number of studies per year 
-df4 <- df2 %>%
-  group_by(`Publication year`) %>%
-  mutate(count_per_year = n())
-
-# create prevalence figure per infection type 
-df5 <- df4 %>%
-  group_by(Group) %>%
-  summarise(count = n()) %>%
-  mutate(Percentage = count / sum(count) * 100)
-
-# create data results
-data_ib <- data.frame(
-  brain_outcome = c('Cerebral lesions', 'White matter lesions', 'White matter microstructure', 'White matter microstructure', 'Anterior cingulate cortex'),
-  finding = c('Significant finding', 'Significant finding' ,'Significant finding', 'Null finding', 'Significant finding'),
-  modality = c('sMRI', 'sMRI', 'DTI', 'DTI', 'fMRI'),
-  count = c(120, 144 ,481, 129, 76),
-  author_names = c('Duggan et al, Tian et al', 'Duggan et al, Yoon et al' ,'Conole et al, Sanders et al, Rasmussen et al', 'Conole et al' ,'Spann et al, Goldstein et al'),
-  developmental_period = c('Infancy, infancy', 'Infancy, infancy', 'Infancy (preterm), infancy, childhood', 'Infancy', 'Infancy, Adulthood')
-)
-data_ib$modality <- factor(data_ib$modality, levels = c("sMRI", "DTI", "fMRI"))
-data_ib$finding <- factor(data_ib$finding)
-
-data_ca <- data.frame(
-  brain_outcome = c('White matter microstructure', 'White matter microstructure', 'White matter lesions', 'White matter lesions', 'Cerebral lesions', 'Cerebral lesions'),
-  finding = c('Significant finding', 'Null finding','Significant finding', 'Null finding','Significant finding', 'Null finding'),
-  modality = c('DTI', 'DTI', 'sMRI', 'sMRI', 'sMRI', 'sMRI'),
-  count = c(210, 92, 573, 157, 740, 388),
-  author_names = c('Anblagan et al, Gaudet et al, Pogribna et al', 'Chau et al', 'Basu et al, Qin et al, Xu et al, Shevell et al', 'Baud et al, Chau et al, Basu et al', 'Budal et al, Jain et al, Jenster et al', 'Granger et al, Qin et al, Xu et al'),
-  developmental_period = c('Infancy (preterm), Infancy (preterm), Infancy (preterm)', 'Infancy (preterm)', 'Infancy (preterm), infancy (preterm), infancy (preterm), childhood (term)', 'Infancy (preterm), Infancy (preterm), infancy (preterm)', 'Infancy (preterm), infancy (preterm), infancy (term)', 'Infancy (preterm), infancy (preterm), infancy (preterm)')
-)
-data_ca$modality <- factor(data_ca$modality, levels = c("sMRI", "DTI"))
-data_ca$finding <- factor(data_ca$finding)
-
-data_ci <- data.frame(
-  brain_outcome = c('Cerebral lesions', 'Cerebral lesions', 'White matter lesions', 'White matter lesions'),
-  finding = c('Significant finding', 'Null finding', 'Significant finding', 'Null finding'),
-  modality = c('sMRI'),
-  count = c(320, 58, 505, 464),
-  author_names = c('Kurokawa et al, McKissic et al, Zeng et al, Capretti et al, Oosterom et al, Roee et al, Diebler et al, Turner et al', 'Roee et al, Virkola et al', 'McKissic et al, Capretti et al, Diebler et al', 'Dammann et al'),
-  developmental_period = c('Infancy, infancy, infancy, infancy, infancy, infancy,infancy,infancy', 'Infancy, infancy', 'Infancy, infancy, infancy', 'Infancy')
-)
-data_ci$modality <- factor(data_ci$modality, levels = c("sMRI"))
-data_ci$finding <- factor(data_ci$finding)
+data_ib <- readxl::read_excel("data_ib.xlsx")
+data_ca <- readxl::read_excel("data_ca.xlsx")
+data_ci <- readxl::read_excel("data_ci.xlsx")
 
 # server
 server <- function(input, output) {
